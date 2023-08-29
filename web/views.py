99de166ca19cc3633e6bd361e50db83a9e3a2e52
manuel matias zubieta <manuelzubieta14@gmail.com>
2023-08-29 10:02:04 -0300

    Changing api to show proper messages

--- web/views.py
+++ web/views.py
@@ -1,6 +1,6 @@
 from .models import Solution
 
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseBadRequest
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
@@ -19,9 +19,9 @@ def jugs_riddle_api(request):
         x, y, z = int(request.POST.get("x")), int(
             request.POST.get("y")), int(request.POST.get("z"))
     except TypeError:
-        return JsonResponse({"steps": []})
+        return HttpResponseBadRequest("Some argument was not passed correctly")
     if any(elem < 0 or elem == 0 for elem in [x, y, z]):
-        return JsonResponse({"steps": []})
+        return HttpResponseBadRequest("All arguments must be greater than zero")
     sol = Solution(x, y, z)
     steps = sol.minSteps()
     return JsonResponse({"steps": steps})
